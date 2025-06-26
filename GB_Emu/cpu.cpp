bf9e38340f41a347d5857f3fd821a00086ec7a80
Louis Stockdale-Lees <lslees1346@gmail.com>
2025-06-26 21:16:59 +0100

    ld r, r

--- GB_Emu/cpu.cpp
+++ GB_Emu/cpu.cpp
@@ -47,6 +47,8 @@ void emulateCycle() {
 
     case 0x00: break; // nop
 
+
+        // byte increments
     case 0x04: regs.B = inc8(regs.B); break; // inc b
     case 0x05: regs.B = dec8(regs.B); break; // dec b
     case 0x0C: regs.C = inc8(regs.C); break; // inc c
@@ -56,7 +58,7 @@ void emulateCycle() {
     case 0x24: regs.H = inc8(regs.H); break; // inc h
     case 0x25: regs.H = dec8(regs.H); break; // dec h
 
-
+        // load immediate byte
     case 0x06: regs.B = memory[regs.PC++]; break; // ld b, n
     case 0x0E: regs.C = memory[regs.PC++]; break; // ld c, n
     case 0x16: regs.D = memory[regs.PC++]; break; // ld d, n
@@ -66,38 +68,7 @@ void emulateCycle() {
     case 0x3E: regs.A = memory[regs.PC++]; break; // ld a, n
 
 
-    case 0x01: // ld bc, n
-    {
-        uint8_t lo = memory[regs.PC++];
-        uint8_t hi = memory[regs.PC++];
-        regs.setBC((hi << 8) | lo);
-        break;
-    }
-
-    case 0x11: // ld de, n
-    {
-        uint8_t lo = memory[regs.PC++];
-        uint8_t hi = memory[regs.PC++];
-        regs.setDE((hi << 8) | lo);
-        break;
-    }
-
-    case 0x21: // ld hl, n
-    {
-        uint8_t lo = memory[regs.PC++];
-        uint8_t hi = memory[regs.PC++];
-        regs.setHL((hi << 8) | lo);
-        break;
-    }
-
-    case 0x31: // ld sp, n
-    {
-        uint8_t lo = memory[regs.PC++];
-        uint8_t hi = memory[regs.PC++];
-        regs.SP = (hi << 8) | lo;
-        break;
-    }
-
+        // 16 bit increments
     case 0x03: // inc bc
     {
         uint16_t x = regs.BC();
@@ -120,13 +91,169 @@ void emulateCycle() {
     }
 
     case 0x33: // inc sp
+    {
         regs.SP++;
         break;
+    }
+
+
+
+        // load immediate 16 bits
+    case 0x01: // ld bc, n
+    {
+        uint8_t lo = memory[regs.PC++];
+        uint8_t hi = memory[regs.PC++];
+        regs.setBC((hi << 8) | lo);
+        break;
+    }
+    case 0x11: // ld de, n
+    {
+        uint8_t lo = memory[regs.PC++];
+        uint8_t hi = memory[regs.PC++];
+        regs.setDE((hi << 8) | lo);
+        break;
+    }
+    case 0x21: // ld hl, n
+    {
+        uint8_t lo = memory[regs.PC++];
+        uint8_t hi = memory[regs.PC++];
+        regs.setHL((hi << 8) | lo);
+        break;
+    }
+    case 0x31: // ld sp, n
+    {
+        uint8_t lo = memory[regs.PC++];
+        uint8_t hi = memory[regs.PC++];
+        regs.SP = (hi << 8) | lo;
+        break;
+    }
+ 
+
+
+
+
+
+    case 0xEA: // ld (a16), a
+    {
+        uint8_t lo = memory[regs.PC++];
+        uint8_t hi = memory[regs.PC++];
+        memory[(hi << 8) | lo] = regs.A;
+        break;
+    }
+    case 0xFA: // ld a, (a16)
+    {
+        uint8_t lo = memory[regs.PC++];
+        uint8_t hi = memory[regs.PC++];
+        regs.A = memory[(hi << 8) | lo];
+        break;
+    }
+
+
+
+
+    case 0xF2: { regs.A = memory[0xFF00 + regs.C]; break; } // ld a, (c)
+    case 0xE2: { memory[0xFF00 + regs.C] = regs.A; break; } // ld (c), a
+
+
+
+    case 0x2A: { regs.A = memory[regs.HL()]; regs.setHL(regs.HL() + 1); break; } // ld a, (hl+)
+    case 0x3A: { regs.A = memory[regs.HL()]; regs.setHL(regs.HL() - 1); break; } // ld a, (hl-)
+    case 0x22: { memory[regs.HL()] = regs.A; regs.setHL(regs.HL() + 1); break; } // ld (hl+), a
+    case 0x32: { memory[regs.HL()] = regs.A; regs.setHL(regs.HL() - 1); break; } // ld (hl-), a
+             
+
+
+
+
+             // ld a, r
+    case 0x78: { regs.A = regs.B; break; } // ld a, b
+    case 0x79: { regs.A = regs.C; break; } // ld a, c
+    case 0x7A: { regs.A = regs.D; break; } // ld a, d
+    case 0x7B: { regs.A = regs.E; break; } // ld a, e
+    case 0x7C: { regs.A = regs.H; break; } // ld a, h
+    case 0x7D: { regs.A = regs.L; break; } // ld a, l
+    case 0x7E: { regs.A = memory[regs.HL()]; break; } // ld a, (hl)
+    case 0x7F: { regs.A = regs.A; break; } // ld a, a
+
+             // ld b, r
+    case 0x40: { regs.B = regs.B; break; } // ld b, b
+    case 0x41: { regs.B = regs.C; break; } // ld b, c
+    case 0x42: { regs.B = regs.D; break; } // ld b, d
+    case 0x43: { regs.B = regs.E; break; } // ld b, e
+    case 0x44: { regs.B = regs.H; break; } // ld b, h
+    case 0x45: { regs.B = regs.L; break; } // ld b, l
+    case 0x46: { regs.B = memory[regs.HL()]; break; } // ld b, (hl)
+    case 0x47: { regs.B = regs.A; break; } // ld b, a
+
+             // ld c, r
+    case 0x48: { regs.C = regs.B; break; } // ld c, b
+    case 0x49: { regs.C = regs.C; break; } // ld c, c
+    case 0x4A: { regs.C = regs.D; break; } // ld c, d
+    case 0x4B: { regs.C = regs.E; break; } // ld c, e
+    case 0x4C: { regs.C = regs.H; break; } // ld c, h
+    case 0x4D: { regs.C = regs.L; break; } // ld c, l
+    case 0x4E: { regs.C = memory[regs.HL()]; break; } // ld c, (hl)
+    case 0x4F: { regs.C = regs.A; break; } // ld c, a
+
+             // ld d, r
+    case 0x50: { regs.D = regs.B; break; } // ld d, b
+    case 0x51: { regs.D = regs.C; break; } // ld d, c
+    case 0x52: { regs.D = regs.D; break; } // ld d, d
+    case 0x53: { regs.D = regs.E; break; } // ld d, e
+    case 0x54: { regs.D = regs.H; break; } // ld d, h
+    case 0x55: { regs.D = regs.L; break; } // ld d, l
+    case 0x56: { regs.D = memory[regs.HL()]; break; } // ld d, (hl)
+    case 0x57: { regs.D = regs.A; break; } // ld d, a
+
+             // ld e, r
+    case 0x58: { regs.E = regs.B; break; } // ld e, b
+    case 0x59: { regs.E = regs.C; break; } // ld e, c
+    case 0x5A: { regs.E = regs.D; break; } // ld e, d
+    case 0x5B: { regs.E = regs.E; break; } // ld e, e
+    case 0x5C: { regs.E = regs.H; break; } // ld e, h
+    case 0x5D: { regs.E = regs.L; break; } // ld e, l
+    case 0x5E: { regs.E = memory[regs.HL()]; break; } // ld e, (hl)
+    case 0x5F: { regs.E = regs.A; break; } // ld e, a
+
+             // ld h, r
+    case 0x60: { regs.H = regs.B; break; } // ld h, b
+    case 0x61: { regs.H = regs.C; break; } // ld h, c
+    case 0x62: { regs.H = regs.D; break; } // ld h, d
+    case 0x63: { regs.H = regs.E; break; } // ld h, e
+    case 0x64: { regs.H = regs.H; break; } // ld h, h
+    case 0x65: { regs.H = regs.L; break; } // ld h, l
+    case 0x66: { regs.H = memory[regs.HL()]; break; } // ld h, (hl)
+    case 0x67: { regs.H = regs.A; break; } // ld h, a
+
+             // ld l, r
+    case 0x68: { regs.L = regs.B; break; } // ld l, b
+    case 0x69: { regs.L = regs.C; break; } // ld l, c
+    case 0x6A: { regs.L = regs.D; break; } // ld l, d
+    case 0x6B: { regs.L = regs.E; break; } // ld l, e
+    case 0x6C: { regs.L = regs.H; break; } // ld l, h
+    case 0x6D: { regs.L = regs.L; break; } // ld l, l
+    case 0x6E: { regs.L = memory[regs.HL()]; break; } // ld l, (hl)
+    case 0x6F: { regs.L = regs.A; break; } // ld l, a
+
+             // ld (hl), r
+    case 0x70: { memory[regs.HL()] = regs.B; break; } // ld (hl), b
+    case 0x71: { memory[regs.HL()] = regs.C; break; } // ld (hl), c
+    case 0x72: { memory[regs.HL()] = regs.D; break; } // ld (hl), d
+    case 0x73: { memory[regs.HL()] = regs.E; break; } // ld (hl), e
+    case 0x74: { memory[regs.HL()] = regs.H; break; } // ld (hl), h
+    case 0x75: { memory[regs.HL()] = regs.L; break; } // ld (hl), l
+    case 0x77: { memory[regs.HL()] = regs.A; break; } // ld (hl), a
+
+
+
+
+
+
 
 
     default:
     {
-        std::cerr << "Unknown opcode: 0x" << std::hex << (int)opcode << "\n";
+        std::cerr << "unknown opcode: 0x" << std::hex << (int)opcode << "\n";
         std::exit(1);
         break;
     }
